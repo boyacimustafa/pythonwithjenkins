ffb63682286ecfb0ed6e77a50a6daaf90b12c5c1
Mustafa Boyar <boyar_mustafa@yahoo.com>
2022-02-15 23:03:42 -0700

    jenkinsfile added and jenkins server configured for it

--- pythonwithjenkins.py
+++ pythonwithjenkins.py
@@ -1,3 +1,4 @@
 print("This is my first project with python and jenkins")
 print("This build was created with github webhooks with jenkins!!")
 print("This build was created with github webhooks with jenkins!! and will be built on a jenkins agent!")
+print("This build was created with github webhooks with jenkins!! and will be built on a jenkins agent! and jenkinsfile will be pulled from my repository")
