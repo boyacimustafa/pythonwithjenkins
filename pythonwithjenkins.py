030dab1637278a2b80f309ed32e28d6d14343dd0
Mustafa Boyar <boyar_mustafa@yahoo.com>
2022-02-15 22:38:30 -0700

    take2

--- pythonwithjenkins.py
+++ pythonwithjenkins.py
@@ -1,3 +1,3 @@
 print("This is my first project with python and jenkins")
 print("This build was created with github webhooks with jenkins!!")
-print("This build was created with github webhooks with jenkins!! and will be built on a jenkins agent")
+print("This build was created with github webhooks with jenkins!! and will be built on a jenkins agent!")
