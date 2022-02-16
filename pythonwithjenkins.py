f1cc9b9e9ba9365d824f99feaf9e0128b98719f1
Mustafa Boyar <boyar_mustafa@yahoo.com>
2022-02-15 22:30:57 -0700

    try the new jenkins agent

--- pythonwithjenkins.py
+++ pythonwithjenkins.py
@@ -1,2 +1,3 @@
 print("This is my first project with python and jenkins")
 print("This build was created with github webhooks with jenkins!!")
+print("This build was created with github webhooks with jenkins!! and will be built on a jenkins agent")
